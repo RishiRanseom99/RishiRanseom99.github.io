cf8602f280fe4c0f160b116e9b31d080e3770afa
RishiRanseom99 <41407371+RishiRanseom99@users.noreply.github.com>
2020-06-03 13:18:59 +0530

    Add files via upload

---
+++ dyanamic.js
@@ -0,0 +1,29 @@
+
+var total=0;
+function addmovie(movie,money=40){
+   
+total=total+money;
+    var listnode= document.getElementById("list");
+       
+    var li=document.createElement("li");
+    
+    li.setAttribute('id',movie);
+    li.appendChild(document.createTextNode(movie+"------->  "+money));
+    listnode.appendChild(li);
+    document.getElementById("total").innerHTML=total;
+  
+}
+function remove(movie)
+{
+    
+    var listnode= document.getElementById("list");
+    var item=document.getElementById(movie);
+    if(item!=null)
+        {listnode.removeChild(item);
+        total=total-40;}
+    else{
+
+        alert("delete pressed");
+    }
+
+}
